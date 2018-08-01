47debae55cb977a9fa391bc6223c9de797dde81f
Dawn <lxysdc1231@163.com>
2018-08-01 09:08:07 +0800

    导入一个UI管理类

---
+++ DemoClient/Assets/Scripts/UI/LoginPanel.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoginPanel : PanelBase
+{
+    //记录当前切换场景的名称
+    public static string loadName="Main";
+    public static string LoginInID;
+    //UI
+    private InputField idInput;
+    private InputField pwInput;
+    private Button loginBtn;
+    private Button regBtn;
+
+    #region 生命周期
+    //初始化
+    public override void Init(params object[] args)
+    {
+        base.Init(args);
+        skinPath = "LoginPanel";
+        layer = PanelLayer.Panel;
+    }
+
+    public override void OnShowing()
+    {
+        base.OnShowing();
+        Transform skinTrans = skin.transform;
+        idInput = skinTrans.Find("Input_UserID").GetComponent<InputField>();
+        pwInput = skinTrans.Find("Input_Passwaord").GetComponent<InputField>();
+        loginBtn = skinTrans.Find("Btn_Login").GetComponent<Button>();
+        regBtn = skinTrans.Find("Btn_Registered").GetComponent<Button>();
+
+        loginBtn.onClick.AddListener(OnLoginClick);
+        regBtn.onClick.AddListener(OnRegClick);
+    }
+    #endregion
+    public void OnRegClick()
+    {
+        PanelMgr.instance.OpenPanel<RegPanel>("");
+        Close();
+    }
+
+    public void OnLoginClick()
+    {
+        //用户名密码为空
+        if (idInput.text == "" || pwInput.text == "")
+        {
+            Debug.Log("用户名密码不能为空!");
+            return;
+        }
+    }
+}
---
+++ DemoClient/Assets/Scripts/UI/RegPanel.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RegPanel : PanelBase
+{
+    //面板按钮
+    private InputField idInput;
+    private InputField pwInput;
+    private Button regBtn;
+    private Button closeBtn;
+
+    #region 生命周期
+    //初始化
+    public override void Init(params object[] args)
+    {
+        base.Init(args);
+        skinPath = "RegPanel";
+        layer = PanelLayer.Panel;
+    }
+
+    public override void OnShowing()
+    {
+
+        base.OnShowing();
+        Transform skinTrans = skin.transform;
+        //获取面板组件
+        idInput = skinTrans.Find("IDInput").GetComponent<InputField>();
+        pwInput = skinTrans.Find("PWInput").GetComponent<InputField>();
+        regBtn = skinTrans.Find("RegBtn").GetComponent<Button>();
+        closeBtn = skinTrans.Find("CloseBtn").GetComponent<Button>();
+        //添加回调监听
+        regBtn.onClick.AddListener(OnRegClick);
+        closeBtn.onClick.AddListener(OnCloseClick);
+    }
+    #endregion
+
+    //注册点击
+    public void OnRegClick()
+    {
+       
+    }
+
+    //注册回调
+    public void OnRegBack()
+    {
+       
+    }
+
+    //回调方法
+    public void OnCloseClick()
+    {
+        PanelMgr.instance.OpenPanel<LoginPanel>("");
+        Close();
+    }
+
+}
