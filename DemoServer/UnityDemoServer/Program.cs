b93e0f2a1c4fcb89e0a74faf30f422e82bcc00d6
sunny <sunny.bao@qq.com>
2018-08-02 14:26:53 +0800

    修

--- DemoServer/UnityDemoServer/Program.cs
+++ DemoServer/UnityDemoServer/Program.cs
@@ -8,18 +8,13 @@ namespace UnityDemoServer
         static void Main(string[] args)
         {
 			var endport = new System.Net.IPEndPoint(System.Net.IPAddress.Any, 26000);
-<<<<<<< HEAD
-            Console.WriteLine(System.Net.IPAddress.Any);
-			System.Net.Sockets.Socket port = new System.Net.Sockets.Socket(endport.AddressFamily, System.Net.Sockets.SocketType.Stream, System.Net.Sockets.ProtocolType.Tcp);
-=======
 			var port = new System.Net.Sockets.Socket(endport.AddressFamily, System.Net.Sockets.SocketType.Stream, System.Net.Sockets.ProtocolType.Tcp);
->>>>>>> d949a35f1379c3d129ba1b87925aee157c9c5dd3
 			port.Bind(endport);
 			port.Listen(2);
 			while (true)
 			{
 				var clientconn = port.Accept();
-				
+				new ClientStatusVariable(new sunny.Sockets.Socket(clientconn));
 			}
         }
     }
